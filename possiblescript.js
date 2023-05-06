ef86b859fdc5c42d2ec50a569e0b7bf00387abea
pattid23 <131926266+pattid23@users.noreply.github.com>
2023-05-05 21:32:19 -0400

    editing the current temperature HTML/JSS

--- possiblescript.js
+++ possiblescript.js
@@ -1,5 +1,15 @@
 function weatherNow(response) {
-  console.log(response.data.main.temp);
+  console.log(response.data);
+  let currentTemperatureElement = document.querySelector("#current-temp");
+  let cityElement = document.querySelector("#display-input");
+  //let conditionsElement = document.querySelector("#conditions");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#windy");
+  currentTemperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  //conditionsElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "e75d849f74609cf49f1546fd56024af1";
